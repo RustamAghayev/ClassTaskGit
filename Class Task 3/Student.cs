9f1b7e54f0814b8aa1344b463b61405be78f25d8
Rüstem Agayev <agayevrustem708@gmail.com>
2023-03-11 13:51:53 +0400

    Add

---
+++ Class Task 3/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RustamAghayevTask3
+{
+    internal class Student
+    {
+        public string name;
+        public string surname;
+        public string speciality;
+        public string groupNo;
+        public int age;
+
+        public override string ToString()
+        {
+          return ($"{name}{surname}{speciality}{groupNo}{age}");
+        }
+    }
+}       
